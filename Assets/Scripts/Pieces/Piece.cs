d686cdb421d36f1d766a34fd28b8009651d8b3fa
sisimonis <39210425+sisimonis@users.noreply.github.com>
2023-05-18 19:40:37 +0200

    piece update with sprites

--- Assets/Scripts/Pieces/Piece.cs
+++ Assets/Scripts/Pieces/Piece.cs
@@ -5,5 +5,7 @@ using UnityEngine;
 
 public abstract class Piece : ScriptableObject
 {
+    public Sprite m_Sprite;
+
     public abstract (int, int)[] computeNewDirections((int, int) sourceDirection);
 }
