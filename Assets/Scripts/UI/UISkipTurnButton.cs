c1aa1f0ac0906c1a0d09c101cb4efb0a21cc310b
sisimonis <39210425+sisimonis@users.noreply.github.com>
2023-06-10 19:55:24 +0200

    move coroutine to SkipButton

--- Assets/Scripts/UI/UISkipTurnButton.cs
+++ Assets/Scripts/UI/UISkipTurnButton.cs
@@ -5,15 +5,42 @@ using UnityEngine;
 public class UISkipTurnButton : MonoBehaviour
 {
     private TurnManager TurnManager;
+    private DataManager DataManager;
 
-    private void Update()
+    private float m_CurrentCooldown;
+
+    private void Awake()
     {
-        TurnManager = FindObjectOfType<TurnManager>();
+        DataManager = FindObjectOfType<DataManager>();
+    }
+
+    private void Start()
+    {
+        TurnManager = DataManager.TurnManager;
     }
 
 
     public void OnClick()
     {
         TurnManager.TrySkipTurn(false);
+    }
+
+    public IEnumerator Cooldown(float cooldown, bool laser)
+    {
+        m_CurrentCooldown = cooldown;
+        while (m_CurrentCooldown > 0)
+        {
+            m_CurrentCooldown -= Time.deltaTime;
+            yield return null;
+        }
+        if (laser)
+        {
+            TurnManager.EndOfLaser(false);
+        }
+        else
+        {
+            TurnManager.m_CanSkipTurn = true;
+        }
+
     }
 }
