b61b43fd53a441e442567c7c90416db854690c82
Antony Zahran <antony.zahran@grenoble-inp.org>
2023-07-22 20:27:12 +0200

    Ajout de la pièce Wall

---
+++ Assets/Scripts/Pieces/Wall.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Wall : Piece
+{
+    public override IEnumerable<Vector2Int> ComputeNewDirections(Vector2Int sourceDirection)
+    {
+        return Enumerable.Empty<Vector2Int>();
+    }
+}
